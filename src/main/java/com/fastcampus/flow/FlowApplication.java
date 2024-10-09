e400905371ba611d763db2a7c3ed36b9778235aa
dongchancho <deamp@naver.com>
2024-10-09 12:30:09 +0900

    레디스 초기설정

--- src/main/java/com/fastcampus/flow/FlowApplication.java
+++ src/main/java/com/fastcampus/flow/FlowApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class FlowApplication implements ApplicationListener<ApplicationReadyEvent> {
+	private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowApplication.class, args);
@@ -16,6 +18,6 @@ public class FlowApplication implements ApplicationListener<ApplicationReadyEven
 
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
-
+		reactiveRedisTemplate.opsForValue().set("testkey", "testValue").subscribe();
 	}
 }
